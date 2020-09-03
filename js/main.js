83ec43b5e18ab826cc2149864ea210c23b41da6c
andrewjspivey <12spivey@gmail.com>
2020-09-02 18:59:23 -0700

    starting to touch up html to get start working

--- js/main.js
+++ js/main.js
@@ -4,15 +4,17 @@
 /* Breakdown:
 - draw field lines on canvas -done
 - get box that will become sprite image to move with arrow keys- done
-- create classes for user player and opponents
-these classes will need functions for players to move 
-- create function for being "tackled",
+- create classes for user player and opponents 
+these classes will need functions for players to move - class for defenders is done, will go back to user if time
+- create function for being "tackled", 
 -  create function for scoring TD,
 - create function to place user and opponents on field and begin moving
 - create functions for menu buttons to start game and look at how to play
 - create function for scoreboard updating 
 - 
  */
+
+
 
 // drawing lines on the field to create canvas
 let canvas = document.querySelector('canvas'); //selected canvas stored in canvas
@@ -23,20 +25,6 @@ canvas.height = 720;
 
 const c = canvas.getContext('2d'); // setting canvas context to 2d
 
-
-
-// c.fillRect(x, y, width, height)
-// creating football field lines
-/* c.fillStyle = "#AA0000";
-c.fillRect(0, 0, 400, 60); // creates top endzone
-c.fillStyle = "#AA0000"; //creates bottom endzone
-c.fillRect(0, 660, 400, 60); */
-
-/* c.beginPath(); // initiates a line to be drawn
-c.moveTo(0, 60); // tells line where to start on canvas moveTo(x, y)
-c.lineTo(400, 60); // tells where to draw the line to from moveTo lineTo(x, y)
-c.strokeStyle = "white" // tells line to be white
-c.stroke(); // tells line to be drawn or shown */ 
 
 // draws lines on the field with for loop
 
@@ -57,121 +45,8 @@ const createField = function () {
     }     
     
 };
-//createField();
 
-
-// creating box that represents player to move with keys
-// Key codes: left arrow is 37 and right arrow is 39, up arraow is 38 and down arrow is 40
 // drawImage method: drawImage(imgsrc, srcx, srcy, srcw,scrh, destx, desty, destw, desth)
-
-/* canvas.addEventListener('keydown', move, true) */
-
-
-/* let xPos = 175;
-let yPos = 650;
-
-let box = c.fillRect(xPos, yPos, 40, 40);
-c.stroke(); */ 
-
-//let userSprite = new Image();
-//userSprite.src = 'https://jvin825.files.wordpress.com/2012/07/all_football_sprite.jpg';
-
-/* userSprite.onload = function() {
-  initDraw();
-};
-
-
-const initDraw = function () {
-    c.drawImage(userSprite, 25, 50, 50, 95, 170, 600, 50, 100);
-} */
-
-// function init() {
-   // c.drawImage(userSprite, 25, 50, 50, 95, 170, 600, 50, 100)
-//}
-
-/* let xPos = 175;
-let yPos = 650;
-
-let box = c.fillRect(xPos, yPos, 40, 40);
-c.stroke(); 
-
-
-function move(e) {
-    
-    if (e.keyCode == 38){ // move box up
-        yPos -= 10;
-        
-    }
-    if (e.keyCode == 37){ // move box left
-        xPos -= 10;
-        
-    }
-    if (e.keyCode == 40){ // move box down
-        yPos += 10;
-        
-    }
-    if (e.keyCode == 39){ // move box right
-        xPos += 10;
-    
-    }
-    canvas.width = canvas.width;
-    createField();
-    c.fillRect(xPos, yPos, 40, 40);
-    c.stroke();
-}
-document.onkeydown = move;  */ 
-
-//creating user controlled player class
-
-/*class User {
-    constructor(image, speed) {
-        this.image = image;
-        this.speed = speed;
-        this.xPos = 170;
-        this.yPos = 600; 
-
-    }
-    initImage() {
-        c.drawImage(this.image, 25, 50, 50, 95, this.xPos, this.yPos, 50, 100);
-    }
-     move() {
-        document.addEventListener('keydown', function(e) {
-            if (e.keyCode == 38){ // move player up
-                this.yPos -= 10;
-                c.drawImage(userSprite, 25, 50, 50, 95, this.xPos, this.yPos, 50, 100);
-            }
-            else if (e.keyCode == 37){ // move player left
-                this.xPos -= 10;
-                c.drawImage(userSprite, 25, 50, 50, 95, this.xPos, this.yPos, 50, 100);
-            }
-            else if (e.keyCode == 40){ // move player down
-                this.yPos += 10;
-                c.drawImage(userSprite, 25, 50, 50, 95, this.xPos, this.yPos, 50, 100);
-            }
-            else if (e.keyCode == 39){ // move player right
-                this.xPos += 10;
-                c.drawImage(userSprite, 25, 50, 50, 95, this.xPos, this.yPos, 50, 100);
-        
-            }
-            canvas.width = canvas.width;
-            createField(); 
-            this.initDraw();
-            c.drawImage(userSprite, 25, 50, 50, 95, this.xPos, this.yPos, 50, 100);
-    }); 
-    //document.onkeydown = this.move;
-    }
-    display() {
-        c.drawImage(userSprite, 25, 50, 50, 95, this.xPos, this.yPos, 50, 100);
-    } 
-
-}; 
-};*/
-
-const player = {
-    x: 175,
-    y: 600, 
-    speed: 10,
-}
 
 
 const userSprite = new Image();
@@ -181,41 +56,32 @@ const defSprite = new Image();
 defSprite.src = 'assets/defender-stepright.png';
 console.log(defSprite)
 
+//player (user) object
+const player = {
+    x: 175, // x = user images x starting point on canvas
+    y: 600, // y - user images y starting point on canvas
+    speed: 10,
+}
 
+// function to draw the player's sprite. params are x, y, width, and height dimensions of sprite img and where on canvas
 function drawSprite(img, srcX, srcY, srcW, srcH, destX, destY, destW, desth) {
     c.drawImage(img, srcX, srcY, srcW, srcH, destX, destY, destW, desth);
 }
 
-//drawSprite(defSprite, 0, 0, 80, 88, 175, 300, 80, 88);
-
-/* function animate() { // will use to change picture of player every step if have time and canvas every time
-    c.clearRect(0, 0, 400, 720) // clears the whole canvas every frame
-    createField(); // recreates field lines every frame
-    drawSprite(userSprite, 0, 0, 50, 95, player.x, player.y, 35, 70); //draws sprites every frame
-    drawSprite(defSprite, 0, 0, 80, 88, 160, 150, 50, 52);
-    defender1.drawSprite;
-    defender1.move;
-    requestAnimationFrame(animate);
-}
-animate();
- */
+// event listener for key control of user
 
 document.addEventListener("keydown", function(event) { // event listener for player movement
     if (event.keyCode == 38){ // move player up
         player.y -= player.speed;
-        //drawSprite(userSprite, 25, 50, 50, 95, player.x, player.y, 50, 100);
     }
     else if (event.keyCode == 37){ // move player left
         player.x -= player.speed;
-        //drawSprite(userSprite, 25, 50, 50, 95, player.x, player.y, 50, 100);
     }
     else if (event.keyCode == 40){ // move player down
         player.y += player.speed;
-        //drawSprite(userSprite, 25, 50, 50, 95, player.x, player.y, 50, 100);
     }
     else if (event.keyCode == 39){ // move player right
         player.x += player.speed;
-        //drawSprite(userSprite, 25, 50, 50, 95, player.x, player.y, 50, 100);
     }
 });
 
@@ -223,58 +89,90 @@ document.addEventListener("keydown", function(event) { // event listener for pla
 //creating defender class to add multiple defenders
 
 class Defender {
-    constructor(num, posX, posY) { //pox and posy are for starting point of defender
-        this.num = num;
+    constructor(posX, posY, speed) { //posX and posY are for starting point of defender
         this.posX = posX;
         this.posY = posY;
-        this.speed = 1.3;
+        this.speed = speed;
     }
-    drawSprite(img, srcX, srcY, srcW, srcH, destX, destY, destW, desth) {
+    drawSprite() { // draw the defender sprites at starting position
         c.drawImage(defSprite,  0, 0, 80, 88, this.posX, this.posY, 50, 52)
     }
-    move() {
+    move() { 
         let randomNum = Math.floor(Math.random() * 100)
         
         if (randomNum >= 50) { // move defender down field 50% of time
             this.posY += this.speed; 
             this.drawSprite();
         }
-        if (randomNum <= 49 && randomNum >= 30) { // move defender down field 50% of time
+        if (randomNum <= 49 && randomNum >= 30) { // move defender left 20% of time
             this.posX -= this.speed; 
             this.drawSprite();
         }
-        if (randomNum <= 29 && randomNum >= 10) { // move defender down field 50% of time
+        if (randomNum <= 29 && randomNum >= 10) { // move defender right 20% of time
             this.posX += this.speed; 
             this.drawSprite();
         }
-        if (randomNum < 10) { // move defender down field 50% of time
+        if (randomNum < 10) { // move defender up field 10% of time
             this.posY -= this.speed; 
             this.drawSprite();
         }
     }
+    
+};
+
+// give defenders different starting positions and different speeds
+const defender1 = new Defender(75, 200, 1.2); 
+const defender2 = new Defender(150, 175, 2);
+const defender3 = new Defender(225, 190, 1.3);
+const defender4 = new Defender(300, 200, 1.5);
+
+
+// defenders array to loop through on tackle function
+let defendersArray = [defender1, defender2, defender3, defender4];
+
+// detects if one of defenders and user touch
+function tackle(userPos, defPos) {
+    return !(userPos.x > defPos.posX + 50 || userPos.x + 40 < defPos.posX || userPos.y > defPos.posY + 52 || userPos.y + 70 < 52)
+        
+    }
+// loops through defenders to detect if user has been touched by defender
+function tackleDetection() {
+    
+    for (let i = 0; i < defendersArray.length; i++) {
+        if (tackle(player, defendersArray[i])){
+            
+    } 
+  }  
+}
+//  checks if user has crossed the endzone
+function touchDown() {
+    if(player.y <= 30) {
+        console.log("touchdown!")
+    }
 }
 
-const defender1 = new Defender(1, 100, 200);
-
+/* function resetGame() {
+    animateGame
+} */
+// animates game 
 function animateGame() { // will use to change picture of player every step if have time and canvas every time
     c.clearRect(0, 0, 400, 720) // clears the whole canvas every frame
     createField(); // recreates field lines every frame
     drawSprite(userSprite, 0, 0, 50, 95, player.x, player.y, 35, 70); //draws sprites every frame
     //drawSprite(defSprite, 0, 0, 80, 88, 160, 150, 50, 52);
-    defender1.drawSprite(defSprite, 0, 0, 80, 88, 160, 150, 50, 52);
-    defender1.move();
+    defender1.drawSprite();
+    defender1.move();;            //calling defenders and their functions 
+    defender2.drawSprite();
+    defender2.move();
+    defender3.drawSprite();
+    defender3.move();
+    defender4.drawSprite();
+    defender4.move();
+    tackleDetection();
+    touchDown();
     requestAnimationFrame(animateGame);
 }
+
 animateGame();
 
-//const player = new User(userSprite, 10);
 
-/* const startGame = function () {
-    player.initImage();
-    //player.move();
-    //player.display();
-    console.log(player);
-}; */
-//startGame();
-
-//document.onkeydown = move;
